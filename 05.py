0f37f7c4b28fefa7bdf82112c134659f26e02da7
Morris Jobke <hey@morrisjobke.de>
2016-12-05 10:31:14 +0100

    Day 05 Puzzle 1
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

---
+++ 05.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import hashlib
+
+def hash_starts_with_5_zeros(string):
+    m = hashlib.md5()
+    m.update(string.encode('utf-8'))
+    hash = m.hexdigest()
+
+    if hash[0:5] == '00000':
+        return hash[5]
+
+    return None
+
+def get_next_character(id, number):
+    """
+
+    #>>> from pprint import pprint
+    #>>> pprint(get_next_character('abc', 0))
+    #(3231929, '1')
+    #>>> pprint(get_next_character('abc', 3231930))
+    #(5017308, '8')
+    """
+
+    character = hash_starts_with_5_zeros('%s%i' % (id, number))
+    while character == None:
+        number += 1
+        character = hash_starts_with_5_zeros('%s%i' % (id, number))
+
+    return (number, character)
+
+def get_passcode(id):
+    """
+
+    >>> print(get_passcode('abc'))
+    18f47a30
+    >>> print(get_passcode('ffykfhsq'))
+    18f47a30
+    """
+
+    number = 0
+    passcode = ''
+
+    while len(passcode) < 8:
+        (number, character) = get_next_character(id, number)
+        passcode += character
+        number += 1
+
+    return passcode
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
